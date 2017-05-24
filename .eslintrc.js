a2666886a65037a2a0d6481130dcc770c71325ac
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-05-24 14:28:13 -0600

    completed fs

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: [
+    'ryansobol/node',
+    'ryansobol/es6',
+    'airbnb'
+  ]
+};
+
+// install all packages required
+
+// npm info "eslint-config-airbnb@latest" peerDependencies
+
+// npm install --save-dev eslint-config-airbnb eslint@^3.19.0 eslint-plugin-jsx-a11y@^5.0.3 eslint-plugin-import@^2.3.0 eslint-plugin-react@^7.0.1
+
+// npm install -D eslint eslint-config-ryansobol
+
+// touch .eslintrc.js
+
+// copy into .eslintrc.js:
+//     module.exports = {
+//       extends: [
+//         'ryansobol/node',
+//         'ryansobol/es6',
+//         'airbnb'
+//       ]
+//     };
+
+// npm run lint pets.js
--- pets.js
+++ pets.js
@@ -1,7 +1,9 @@
-//eslint-config-airbnb
+#!/usr/bin/env node
+// $ chmod +x pets.js
+
 
 const fs = require('fs');
-const path = require('path');
+
 const cmd = process.argv[2];
 const index = process.argv[3];
 
@@ -13,96 +15,86 @@ if (!cmd) {
     if (err) {
       throw err;
     }
-    var pets;
-    if (index){
-      if (JSON.parse(data)[index]){
-        pets = JSON.parse(data)[index];
+    if (index) {
+      if (JSON.parse(data)[index]) {
+        const pets = JSON.parse(data)[index];
         console.log(pets);
-      }else{
+      } else {
         console.error('Usage: node pets.js read INDEX');
         process.exit(1);
       }
-
-
-    }
-
-    else {
-      pets = JSON.parse(data);
+    } else {
+      const pets = JSON.parse(data);
       console.log(pets);
     }
-
   });
-} else if(cmd === 'create'){
-if(!process.argv[5]){
-console.error('Usage: node pets.js create AGE KIND NAME');
-process.exit(1);
-}
-    fs.readFile('./pets.json','utf8',(readError, data) => {
-      if(readError){
-        throw error
+} else if (cmd === 'create') {
+  if (!process.argv[5]) {
+    console.error('Usage: node pets.js create AGE KIND NAME');
+    process.exit(1);
+  }
+  fs.readFile('./pets.json', 'utf8', (readError, data) => {
+    if (readError) {
+      throw readError;
+    }
+    const pets = JSON.parse(data);
+    const newPet = {
+      age: Number(process.argv[3]),
+      kind: process.argv[4],
+      name: process.argv[5],
+    };
+    pets.push(newPet);
+    const petsJson = JSON.stringify(pets);
+    fs.writeFile('./pets.json', petsJson, (writeError) => {
+      if (writeError) {
+        throw writeError;
       }
-      const pets = JSON.parse(data);
-      const newPet = {
-        age: Number(process.argv[3]),
-        kind: process.argv[4],
-        name: process.argv[5],
-      };
-      pets.push(newPet);
-      const petsJson = JSON.stringify(pets);
-      fs.writeFile('./pets.json',petsJson, (writeError) => {
-        if(writeError){
-          throw writeError
-        }
-        console.log(newPet);
-      })
-    })
-
-
-
-
-} else if(cmd === 'update'){
-if(!process.argv[6]){
-console.error('Usage: node pets.js update INDEX AGE KIND NAME');
-process.exit(1);
-}
-    fs.readFile('./pets.json','utf8',function(readError, data){
-      if(readError){
-        throw error
+      console.log(newPet);
+    });
+  });
+} else if (cmd === 'update') {
+  if (!process.argv[6]) {
+    console.error('Usage: node pets.js update INDEX AGE KIND NAME');
+    process.exit(1);
+  }
+  fs.readFile('./pets.json', 'utf8', (readError, data) => {
+    if (readError) {
+      throw readError;
+    }
+    const pets = JSON.parse(data);
+    const updatePet = {
+      age: Number(process.argv[4]),
+      kind: process.argv[5],
+      name: process.argv[6],
+    };
+    pets[process.argv[3]] = updatePet;
+    const petsJson = JSON.stringify(pets);
+    fs.writeFile('./pets.json', petsJson, (writeError) => {
+      if (writeError) {
+        throw writeError;
       }
-      const pets = JSON.parse(data);
-      const updatePet = {
-        age: Number(process.argv[4]),
-        kind: process.argv[5],
-        name: process.argv[6],
-      };
-      pets[process.argv[3]] = updatePet;
-      const petsJson = JSON.stringify(pets);
-      fs.writeFile('./pets.json',petsJson,function(writeError){
-        if(writeError){
-          throw writeError
-        }
-        console.log(updatePet);
-      })
-    })
-} else if(cmd === 'destroy'){
-  if(!process.argv[3]){
+      console.log(updatePet);
+    });
+  });
+} else if (cmd === 'destroy') {
+  if (!process.argv[3]) {
     console.error('Usage: node pets.js destroy INDEX');
     process.exit(1);
   }
-    fs.readFile('./pets.json','utf8',function(readError, data){
-      if(readError){
-        throw error
+  fs.readFile('./pets.json', 'utf8', (readError, data) => {
+    if (readError) {
+      throw readError;
+    }
+    const i = process.argv[3];
+    const pets = JSON.parse(data);
+    const deletedPet = pets[i];
+    pets.splice(i, 1);
+    const petsJson = JSON.stringify(pets);
+    fs.writeFile('./pets.json', petsJson, (writeError) => {
+      if (writeError) {
+        throw writeError;
       }
-      const index = process.argv[3];
-      const pets = JSON.parse(data);
-      const deletedPet = pets[index];
-      pets.splice(index, 1);
-      const petsJson = JSON.stringify(pets);
-      fs.writeFile('./pets.json',petsJson,function(writeError){
-        if(writeError){
-          throw writeError
-        }
-        console.log(deletedPet);
-      })
-    })
+      console.log(deletedPet);
+    });
+  });
 }
